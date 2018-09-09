5072600d179e92aafc94f6cb70889751693584c8
Ahmed Rizwan <ahmedrizwan@outlook.com>
2018-09-09 19:25:34 +0500

    Add react project and files

---
+++ src/Subtitle.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Subtitle(props: {}) {
+  return (
+    <p className="subtitle" style={{ marginBottom: 16 }}>
+      Software Engineer @SnappyMob
+    </p>
+  );
+}
+
+export default Subtitle;
